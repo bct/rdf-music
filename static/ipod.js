80ea7c99a79d7a2673208370e5000da4ea5b1284
Brendan Taylor <whateley@gmail.com>
2009-03-22 00:13:03 -0600

    queue ajax gnupod requests

--- static/ipod.js
+++ static/ipod.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
+  var ipodQueue = $('<ul id="ipodQueue"></ul>')
+
+  $("body").append(ipodQueue);
+
   $('.album').each(function() {
     var dl = $('<span class="ipodDl">↡</span>');
 
     dl.click(function() {
-      $.post(app_path('ipod'), {album: $(this).parent().attr('about') });
+      var album = $(this).parent();
+      var li = $('<li>' + album.children('.title').text() + '</li>');
+      ipodQueue.append(li);
+
+      ipodQueue.queue(function() {
+        var qi = $(this);
+
+        $.post(app_path('ipod'), {album: album.attr('about') }, function() {
+          li.remove();
+          qi.dequeue();
+        });
+      });
     });
 
     $(this).prepend(dl);
